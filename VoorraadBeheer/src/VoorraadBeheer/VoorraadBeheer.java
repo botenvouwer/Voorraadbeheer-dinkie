c31584c565e252de3e8e503ff2e26bf28040a176
William Loosman <william.wl@live.nl>
2015-03-17 21:48:31 +0100

    dinkie

--- VoorraadBeheer/src/VoorraadBeheer/VoorraadBeheer.java
+++ VoorraadBeheer/src/VoorraadBeheer/VoorraadBeheer.java
@@ -1,16 +1,34 @@
 package VoorraadBeheer;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class VoorraadBeheer extends Application{
 
     public static void main(String[] args){
-        System.out.println("test");
+        launch(args);
     }
     
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage applicatie) throws Exception {
+        
+        Button toevoegen = new Button("Artikel toevoegen");
+        
+        HBox controls = new HBox();
+        controls.getChildren().addAll(toevoegen);
+        
+        BorderPane pane = new BorderPane();
+        pane.setBottom(controls);
+        
+        Scene scherm = new Scene(pane);
+        
+        applicatie.setScene(scherm);
+        applicatie.sizeToScene();
+        applicatie.show();
     }
     
 }
