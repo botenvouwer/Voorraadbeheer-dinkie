d712d4e47995b7bf106b5ddf03b1202ee324d34c
William Loosman <william.wl@live.nl>
2015-03-17 20:48:39 +0100

    test william

--- VoorraadBeheer/src/VoorraadBeheer/VoorraadBeheer.java
+++ VoorraadBeheer/src/VoorraadBeheer/VoorraadBeheer.java
@@ -5,6 +5,10 @@ import javafx.stage.Stage;
 
 public class VoorraadBeheer extends Application{
 
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
         
