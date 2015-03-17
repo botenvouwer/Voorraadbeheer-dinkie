a437fd5ef8fbdc7dc1872e5190d626a2aa22d9b4
Deadwalker09 <nigelvh@hotmail.com>
2015-03-17 14:06:49 +0100

    Revert "y7yt"
    This reverts commit 1523149eb0e7cf5862bf2d8154a2ecd1b02f47fe.

--- VoorraadBeheer/src/VoorraadBeheer/Artikel.java
+++ VoorraadBeheer/src/VoorraadBeheer/Artikel.java
@@ -1,31 +1,7 @@
 package VoorraadBeheer;
 
 public class Artikel {
-    private final int artNummer;
-    private final String naam;
-    private int aantalAanwezig;
-    
-    public Artikel(int artNummer, String naam) {
-        this.artNummer = artNummer;
-        this.naam = naam;
-        aantalAanwezig = 0;
-    }
-    
-    public int getArtNummer() {
-        return artNummer;
-    }
-    
-    public String getNaam() {
-        return naam;
-    }
-    
-    public void setAantalAaanwezig(int geteldAantal) {
-        aantalAanwezig = geteldAantal;
-    }
-    
-    public int getAantalAanwezig() {
-        return aantalAanwezig;
-    }
-    
+    private int art_nummer;
+    private String naam;
     
 }
