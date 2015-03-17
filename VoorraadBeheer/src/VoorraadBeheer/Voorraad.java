ce11236d13844de67601ce8131bbeff5fccf352e
Deadwalker09 <nigelvh@hotmail.com>
2015-03-17 22:39:25 +0100

    Voorraad

--- VoorraadBeheer/src/VoorraadBeheer/Voorraad.java
+++ VoorraadBeheer/src/VoorraadBeheer/Voorraad.java
@@ -1,38 +1,61 @@
 package VoorraadBeheer;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class Voorraad {
+
     private ArrayList<Artikel> lijstArtikelen;
-    
+
     public Voorraad() {
         lijstArtikelen = new ArrayList<>();
     }
-    
+
     public boolean bestaadAl(Artikel check) {
         boolean b = false;
-        for(Artikel refArtikel: lijstArtikelen) {
+        for (Artikel refArtikel : lijstArtikelen) {
             b = check.equalss(refArtikel);
-            }
+        }
         return b;
     }
-    
+
     public boolean voegArtikelToe(Artikel newArtikel) {
-        if(!bestaadAl(newArtikel)) {
+        if (!bestaadAl(newArtikel)) {
             lijstArtikelen.add(newArtikel);
             return true;
         } else {
             return false;
         }
     }
-    
+
     public boolean verwijderArtikel(Artikel wegArtikel) {
-        if(!bestaadAl(wegArtikel)) {
+        if (!bestaadAl(wegArtikel)) {
             lijstArtikelen.remove(wegArtikel);
             return true;
         } else {
             return false;
         }
     }
+
+    public void slaOp() throws FileNotFoundException, IOException {
+        FileOutputStream fout = new FileOutputStream("/Database");
+        ObjectOutputStream oos = new ObjectOutputStream(fout);
+        oos.writeObject(lijstArtikelen);
+        fout.close();
+    }
     
+    public void openen() throws FileNotFoundException, IOException, ClassNotFoundException {
+        File f = new File("/Database");
+        if(f.exists()) {
+        FileInputStream fis = new FileInputStream("/Database");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        lijstArtikelen = (ArrayList<Artikel>) ois.readObject();
+        }
+    }
 }
