bbfb46f166dfd27c7a62337304d6e951fe87562f
William Loosman <william.wl@live.nl>
2015-03-18 10:09:02 +0100

    nigel todo

--- VoorraadBeheer/src/VoorraadBeheer/Voorraad.java
+++ VoorraadBeheer/src/VoorraadBeheer/Voorraad.java
@@ -1,31 +1,17 @@
 package VoorraadBeheer;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Voorraad {
-
+    //extenden!
     private ArrayList<Artikel> lijstArtikelen;
-
+    
     public Voorraad() {
         lijstArtikelen = new ArrayList<>();
-        try {
-            openen();
-        } catch (IOException ex) {
-            Logger.getLogger(Voorraad.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Voorraad.class.getName()).log(Level.SEVERE, null, ex);
-        }
     }
-
+    
+    //todo get(Artikel)
+    
     public boolean bestaadAl(Artikel check) {
         boolean b = false;
         for (Artikel refArtikel : lijstArtikelen) {
@@ -49,22 +35,6 @@ public class Voorraad {
             return true;
         } else {
             return false;
-        }
-    }
-
-    public void slaOp() throws FileNotFoundException, IOException {
-        FileOutputStream fout = new FileOutputStream("Database");
-        ObjectOutputStream oos = new ObjectOutputStream(fout);
-        oos.writeObject(lijstArtikelen);
-        fout.close();
-    }
-    
-    public void openen() throws FileNotFoundException, IOException, ClassNotFoundException {
-        File f = new File("Database");
-        if(f.exists()) {
-        FileInputStream fis = new FileInputStream("Database");
-        ObjectInputStream ois = new ObjectInputStream(fis);
-        lijstArtikelen = (ArrayList<Artikel>) ois.readObject();
         }
     }
 }
